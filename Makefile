2d586d2c4f1874e457292a77d6e1b5b3c777794c
Antonio Ercole De Luca <eracle@posteo.eu>
2025-06-07 08:52:56 +0100

    fixed init

--- Makefile
+++ Makefile
@@ -5,12 +5,8 @@
 help: ## shows the list of commands
 	@perl -nle'print $& if m{^[a-zA-Z_-]+:.*?## .*$$}' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-25s\033[0m %s\n", $$1, $$2}'
 
-venv: ## init virtual environment locally
-	python -m venv venv
-	venv/bin/pip install --upgrade pip
-	venv/bin/pip install -r requirements.txt
-
 run: ## launch notebook using local venv
 	./init.sh
+
 compose: ## executes the notebook on docker
 	docker compose -f local.yml up --build
--- init.sh
+++ init.sh
@@ -2,14 +2,15 @@
 set -e
 
 # Create virtual environment if it doesn't exist
-if [ ! -d "venv" ]; then
-    python -m venv venv
-    ./venv/bin/pip install --upgrade pip
-    ./venv/bin/pip install -r requirements.txt
+if [ ! -d ".venv" ]; then
+    python -m venv .venv
+    source .venv/bin/activate
+    pip install --upgrade pip
+    pip install -r requirements.txt
 fi
 
 # Activate the virtual environment
-source venv/bin/activate
+source .venv/bin/activate
 
 # Start the marimo notebook
 marimo edit notebook.py --host 0.0.0.0 --port 8888
